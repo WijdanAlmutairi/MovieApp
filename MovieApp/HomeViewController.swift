5e725abdf2776c74776a48a68296c9fd80901c4e
Wijdan Almutairi <wijdan.s.almutairi@gmail.com>
2022-08-21 19:23:08 +0300

    Add the app classes (Home and Movie Details).

--- MovieApp/HomeViewController.swift
+++ MovieApp/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  MovieApp
 //
 //  Created by Wijdan Almutairi on 21/08/2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
 }
-
---
+++ MovieApp/MovieDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetailsViewController.swift
+//  MovieApp
+//
+//  Created by Wijdan Almutairi on 21/08/2022.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
