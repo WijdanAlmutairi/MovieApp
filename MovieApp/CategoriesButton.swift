ee7e55139438c73f27e668f18de76064dc24da5f
Wijdan Almutairi <wijdan.s.almutairi@gmail.com>
2022-08-25 02:10:54 +0300

    Add the categories with auto layout programatically.

--- MovieApp/CategoriesButton.swift
+++ MovieApp/CategoriesButton.swift
@@ -11,6 +11,10 @@ class CategoriesButton: UIButton {
 
     override init (frame: CGRect){
         super.init(frame: frame)
+        let width = 80
+        let height = 80
+        self.frame.size = CGSize(width: width, height: height)
+        configure()
     }
     
     required init?(coder: NSCoder) {
@@ -19,13 +23,17 @@ class CategoriesButton: UIButton {
     
     init (backgroundColor: UIColor, title: String) {
         super.init(frame: .zero)
+        let width = 80
+        let height = 80
+        self.frame.size = CGSize(width: width, height: height)
         self.backgroundColor = backgroundColor
         self.setTitle(title, for: .normal)
+        configure()
     }
     
     private func configure() {
         layer.cornerRadius = 10
-        titleLabel?.textColor = UIColor(named: "")
+        titleLabel?.textColor = UIColor(named: "categoryBox")
         titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         translatesAutoresizingMaskIntoConstraints = false
     }
--- MovieApp/HomeViewController.swift
+++ MovieApp/HomeViewController.swift
@@ -8,9 +8,45 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    let categoryHorror =  CategoriesButton(backgroundColor: UIColor(named: "categoryBox") ?? .systemBackground, title: "Horror")
+    
+    let categoryComedy =  CategoriesButton(backgroundColor: UIColor(named: "categoryBox") ?? .systemBackground, title: "Comedy")
+    
+    let categoryAction =  CategoriesButton(backgroundColor: UIColor(named: "categoryBox") ?? .systemBackground, title: "Action")
+    
+    let categoryDrama =  CategoriesButton(backgroundColor: UIColor(named: "categoryBox") ?? .systemBackground, title: "Drama")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCategories()
     
+    }
+    
+    func configureCategories() {
+        let stackH = UIStackView(arrangedSubviews: [categoryHorror, categoryComedy, categoryAction, categoryDrama ])
+        stackH.distribution = .fillEqually
+        stackH.axis = .horizontal
+        stackH.spacing = 10
+        stackH.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackH)
+        
+        // set buttons to the function
+        categoryHorror.addTarget(self, action: #selector(pushCategory), for: .touchUpInside)
+        categoryComedy.addTarget(self, action: #selector(pushCategory), for: .touchUpInside)
+        categoryAction.addTarget(self, action: #selector(pushCategory), for: .touchUpInside)
+        categoryDrama.addTarget(self, action: #selector(pushCategory), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            stackH.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25),
+            stackH.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            stackH.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            stackH.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    @objc func pushCategory(_ sender: UIButton) {
+        sender.backgroundColor = UIColor(named: "tint")
     }
 }
